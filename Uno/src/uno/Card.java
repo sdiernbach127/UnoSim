8b1d66c170ab8796e5e122f02cc637ee0f1004ac
U-DIERNBACHTOWER\Spencer <stsld05@moravian.edu>
2016-03-13 22:48:25 -0400

    Made Deck file to be able to read in file. Working on Card to print string of each card

--- Uno/src/uno/Card.java
+++ Uno/src/uno/Card.java
@@ -3,6 +3,20 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+/*
+ * If color int is 
+ * 1 - yellow
+ * 2 - red
+ * 3 - blue
+ * 4 - green
+ * 5 - Wild
+ * 
+ * If value int is 
+ * between 0 and 9 is that value
+ * 10 - skip
+ * 11 - reverse
+ * 12 - draw 2
+ */
 
 // There are 4 colors Possibly 5 if you count the wilds as their own color
 // Counting the different wilds it looks like there are 15 possible values to represent the type of card.
@@ -10,22 +24,56 @@ package uno;
 
 public class Card 
 {
-    private int color;
-    private int value;
+    private int clr;
+    private int val;
     
     public Card(int color, int value)
     {
-        this.color = color;
-        this.value = value;
+        clr = color;
+        val = value;
     }
     
     public int getColor()
     {
-        return this.color;
+        return clr;
     }
     
     public int getValue()
     {
-        return this.value;
+        return val;
     }
+    public String toString()
+  {
+    String clrSt="O";
+    String valSt="O";
+   
+    
+    if (clr==5)
+    {
+      clrSt= "Wild";
+      if (val == 0)
+          valSt = "normal";
+      if (val == 1)
+          valSt = "Draw 4";
+    }
+    else if (clr==1)
+    {
+      clrSt= "Y";
+    }
+    else if (clr==2)
+    {
+      clrSt= "R";
+    }
+     else if (clr==3)
+    {
+      clrSt= "B";
+    }
+     else if (clr==4)
+    {
+      clrSt= "G";
+    }
+    
+    String newstring= (qnt + clrSt+ shdSt + shpSt);
+    return (newstring);
+  }
 }
--- Uno/src/uno/Deck.java
+++ Uno/src/uno/Deck.java
@@ -5,19 +5,114 @@
  */
 package uno;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
 /**
  *
  * @author Spencer Diernbach
  */
 public class Deck {
-    private Card cards[];
+  
+  private ArrayList<Card> theCards;
+  private int nextCardIndex;
+  
+ 
+  public Deck(String filename)
+  {
+    // Create the arraylist so we have a place to put the cards
+    theCards = new ArrayList<Card>(108);
     
-    public Deck(){
-        this.cards = new Card[108];
-        for(int i=0; i < 108 ; i++){
-            Card card = new Card(1,1);
-            this.cards[i] = card;      
-        }
+    try
+    {
+      String line;
+      BufferedReader infile = new BufferedReader(new FileReader(filename));
+      int position = 0;
+  
+      while((line = infile.readLine()) != null)
+      {
+        // Blank lines might contain white space, so trim it off
+        line = line.trim();
+        
+        // ignore blank lines
+        // continue will cause the program to stop executing this iteration
+        // of the body and re-evaluate the condition.
+        if(line.length() == 0)
+          continue;
+        
+        // ignore comments
+        if(line.startsWith("#"))
+          continue;
+            
+        // a valid line contains 2 integers
+        // The StringTokenizer auto-magically breaks a string
+        // into parts
+        StringTokenizer tokenizer = new StringTokenizer(line);
+        
+        // The tokenizer gives us strings, that have to be converted,
+        // or parsed, into integers.  If the string isn't an integer,
+        // it will through an exception
+        int color = Integer.parseInt(tokenizer.nextToken());
+        int value = Integer.parseInt(tokenizer.nextToken());
+        
+        // If there was not an exception in the parse, then we
+        // can create the card and add it to the deck.
+        theCards.add(new Card(color, value));
+      }
     }
+    // We catch exceptions here and through a different kind of exception.
+    // This code will make sense by the end of the semester.  I promise!
+    catch(Exception e)
+    {
+      throw new RuntimeException("Error while reading file: " + e.toString());
+    }
+    
+    // The first card we take is in index 0 (no matter how many cards
+    // were in the file).
+    
+    
+    nextCardIndex = 0;
+  }
+  
+  // Other methods go here.
+  public boolean hasNext()
+  {
+    if(theCards.size()==nextCardIndex)
+    {
+      return false;
+    }
+    else
+    {
+      return true;
+    }
+  }
+  public Card 
+        getNext()
+  {
+    int returnCard=0;
+    if (theCards.size()==nextCardIndex)
+    {
+    return null;
+    }
+    else
+    {
+      returnCard=nextCardIndex;
+      nextCardIndex=nextCardIndex+1;
+      return theCards.get(returnCard);
+      
+    }
+  }
+  public static void main(String[] args)
+  {
+    Deck d= new Deck("UnoDeck.dat");
+    while (d.hasNext())
+    {
+      Card c= d.getNext();
+      System.out.println(c);
+    }
+  }    
     
 }
