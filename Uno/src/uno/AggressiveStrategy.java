2c19987c296c3304dbc1b1b15b01b38ac34cb964
Spencer Diernbach <stsld05@moravian.edu>
2016-03-16 13:50:08 -0400

    commented out print statements in strategies

--- Uno/src/uno/AggressiveStrategy.java
+++ Uno/src/uno/AggressiveStrategy.java
@@ -41,7 +41,7 @@ public class AggressiveStrategy extends Strategy
 
     public void takeCard(Card drawnCard)
     {
-        System.out.println("drawnCard = " + drawnCard);
+        //System.out.println("drawnCard = " + drawnCard);
         hand.add(drawnCard);
     }
     
@@ -59,9 +59,9 @@ public class AggressiveStrategy extends Strategy
         
         for(Card c:hand)
         {
-            System.out.println(c);
+            //System.out.println(c);
             //System.out.println("c index = " + hand.indexOf(c));
-            System.out.println("hand = " + hand.size());
+            //System.out.println("hand = " + hand.size());
             if(c.getColor() == 5)
             {
                 if(c.getValue() == 14)
