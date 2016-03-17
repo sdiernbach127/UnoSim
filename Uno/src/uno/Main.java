694dfea9806200efeaa5c510bff46e9a78b8965d
U-DIERNBACHTOWER\Spencer <stsld05@moravian.edu>
2016-03-16 20:42:27 -0400

    Seperate class for each strategy versus all the others

--- Uno/src/uno/Main.java
+++ Uno/src/uno/Main.java
@@ -14,20 +14,59 @@ public class Main
     public static void main(String[] args)
     {
         int numGames = 10000;
-        Versus versus = new Versus();
+          
+        //color versus all the others
+        ColorVersus colorVersus = new ColorVersus();
+       
+        colorVersus.ColorVsColor(numGames);
+        colorVersus.ColorVsAggressive(numGames);
+        colorVersus.ColorVsPassive(numGames);
+        colorVersus.ColorVsUnit(numGames);
+        colorVersus.ColorVsRandom(numGames);
         
-        versus.ColorVsColor(numGames);
-        versus.ColorVsAgressive(numGames);
-        versus.ColorVsPassive(numGames);
+        System.out.println("\n");
+        System.out.println("============================================================================");
+        //Aggressive versus all the others 
+        AggressiveVersus aggressiveVersus = new AggressiveVersus();
+      
+        aggressiveVersus.AggressiveVsColor(numGames);
+        aggressiveVersus.AggressiveVsAggressive(numGames);
+        aggressiveVersus.AggressiveVsPassive(numGames);
+        aggressiveVersus.AggressiveVsUnit(numGames);
+        aggressiveVersus.AggressiveVsRandom(numGames);
         
-        versus.AgressiveVsAgressive(numGames);
-        versus.AgressiveVsColor(numGames);
+        System.out.println("\n");
+        System.out.println("============================================================================");
+        //Passive versus all the others 
+        PassiveVersus passiveVersus = new PassiveVersus();
+      
+        passiveVersus.PassiveVsColor(numGames);
+        passiveVersus.PassiveVsAggressive(numGames);
+        passiveVersus.PassiveVsPassive(numGames);
+        passiveVersus.PassiveVsUnit(numGames);
+        passiveVersus.PassiveVsRandom(numGames);
         
-        versus.PassiveVsPassive(numGames);
+        System.out.println("\n");
+        System.out.println("============================================================================");
+        //Unit versus all the others 
+        UnitVersus unitVersus = new UnitVersus();
+      
+        unitVersus.UnitVsColor(numGames);
+        unitVersus.UnitVsAggressive(numGames);
+        unitVersus.UnitVsPassive(numGames);
+        unitVersus.UnitVsUnit(numGames);
+        unitVersus.UnitVsRandom(numGames);
         
-        versus.UnitVsUnit(numGames);
-        
-        versus.RandomVsRandom(numGames);
+        System.out.println("\n");
+        System.out.println("============================================================================");
+        //Random versus all the others 
+        RandomVersus randomVersus = new RandomVersus();
+      
+        randomVersus.RandomVsColor(numGames);
+        randomVersus.RandomVsAggressive(numGames);
+        randomVersus.RandomVsPassive(numGames);
+        randomVersus.RandomVsUnit(numGames);
+        randomVersus.RandomVsRandom(numGames);
     }   
     
 }
---
+++ Uno/src/uno/PassiveVersus.java
@@ -0,0 +1,183 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uno;
+
+/**
+ *
+ * @author Spencer
+ */
+public class PassiveVersus {
+    
+    public void PassiveVsColor(int numGames)
+    {
+        int winner;
+        int[] players = new int[3];
+        int totalTurns = 0;
+        int p1TotalDraws = 0;
+        int p2TotalDraws = 0;
+        
+
+        Strategy color = new PassiveStrategy();
+        Strategy color1 = new AggressiveStrategy();
+       
+        Game game = new Game();
+        for(int i = 0; i < numGames; i++)
+        {
+            game = new Game();
+            winner = game.playGame(color, color1);
+            players[winner] += 1; // add to histogram
+            totalTurns += game.getNumTurns();
+            p1TotalDraws += game.getP1Draws();
+            p2TotalDraws += game.getP2Draws();
+            
+            //System.out.println("Strategy " + winner + " wins!");
+        }
+            System.out.println("\n");
+            System.out.println("This was a " + color.getStratName() + " vs " + color1.getStratName() + " game, where " + color.getStratName() + " goes first.");
+            System.out.println("Here's some Statistics:");
+            System.out.println("---------------------------------------------------------------------------");
+            System.out.println("The number of games played is " + numGames);
+            System.out.println("Average turns per game = " + totalTurns/numGames);
+            System.out.println(color.getStratName() + "'s percentage of wins = " + (double)players[1]/(double)numGames + " with an average of " + p1TotalDraws/numGames + " number of draws per game");
+            System.out.println(color1.getStratName() +"'s percentage of wins = " + (double)players[2]/(double)numGames + " with an average of " + p2TotalDraws/numGames + " number of draws per game");
+    }
+    
+    public void PassiveVsAggressive(int numGames)
+    {
+        int winner;
+        int[] players = new int[3];
+        int totalTurns = 0;
+        int p1TotalDraws = 0;
+        int p2TotalDraws = 0;
+      
+        Strategy color = new PassiveStrategy(); 
+        Strategy color1 = new AggressiveStrategy();
+        
+       
+        Game game = new Game();
+        for(int i = 0; i < numGames; i++)
+        {
+            game = new Game();
+            winner = game.playGame(color, color1);
+            players[winner] += 1; // add to histogram
+            totalTurns += game.getNumTurns();
+            p1TotalDraws += game.getP1Draws();
+            p2TotalDraws += game.getP2Draws();
+            
+            //System.out.println("Strategy " + winner + " wins!");
+        }
+            System.out.println("\n");
+            System.out.println("This was a " + color.getStratName() + " vs " + color1.getStratName() + " game, where " + color.getStratName() + " goes first.");
+            System.out.println("Here's some Statistics:");
+            System.out.println("---------------------------------------------------------------------------");
+            System.out.println("The number of games played is " + numGames);
+            System.out.println("Average turns per game = " + totalTurns/numGames);
+            System.out.println(color.getStratName() + "'s percentage of wins = " + (double)players[1]/(double)numGames + " with an average of " + p1TotalDraws/numGames + " number of draws per game");
+            System.out.println(color1.getStratName() +"'s percentage of wins = " + (double)players[2]/(double)numGames + " with an average of " + p2TotalDraws/numGames + " number of draws per game");
+    }
+    
+    public void PassiveVsPassive(int numGames)
+    {
+        int winner;
+        int[] players = new int[3];
+        int totalTurns = 0;
+        int p1TotalDraws = 0;
+        int p2TotalDraws = 0;
+      
+        Strategy color = new PassiveStrategy(); 
+        Strategy color1 = new PassiveStrategy();
+        
+       
+        Game game = new Game();
+        for(int i = 0; i < numGames; i++)
+        {
+            game = new Game();
+            winner = game.playGame(color, color1);
+            players[winner] += 1; // add to histogram
+            totalTurns += game.getNumTurns();
+            p1TotalDraws += game.getP1Draws();
+            p2TotalDraws += game.getP2Draws();
+            
+            //System.out.println("Strategy " + winner + " wins!");
+        }
+            System.out.println("\n");
+            System.out.println("This was a " + color.getStratName() + " vs " + color1.getStratName() + " game, where " + color.getStratName() + " goes first.");
+            System.out.println("Here's some Statistics:");
+            System.out.println("---------------------------------------------------------------------------");
+            System.out.println("The number of games played is " + numGames);
+            System.out.println("Average turns per game = " + totalTurns/numGames);
+            System.out.println(color.getStratName() + "'s percentage of wins = " + (double)players[1]/(double)numGames + " with an average of " + p1TotalDraws/numGames + " number of draws per game");
+            System.out.println(color1.getStratName() +"'s percentage of wins = " + (double)players[2]/(double)numGames + " with an average of " + p2TotalDraws/numGames + " number of draws per game");
+    }
+    
+    public void PassiveVsUnit(int numGames)
+    {
+        int winner;
+        int[] players = new int[3];
+        int totalTurns = 0;
+        int p1TotalDraws = 0;
+        int p2TotalDraws = 0;
+        
+
+        Strategy color = new PassiveStrategy();
+        Strategy color1 = new UnitStrategy();
+               
+        Game game = new Game();
+        for(int i = 0; i < numGames; i++)
+        {
+            game = new Game();
+            winner = game.playGame(color, color1);
+            players[winner] += 1; // add to histogram
+            totalTurns += game.getNumTurns();
+            p1TotalDraws += game.getP1Draws();
+            p2TotalDraws += game.getP2Draws();
+            
+            //System.out.println("Strategy " + winner + " wins!");
+        }
+            System.out.println("\n");
+            System.out.println("This was a " + color.getStratName() + " vs " + color1.getStratName() + " game, where " + color.getStratName() + " goes first.");
+            System.out.println("Here's some Statistics:");
+            System.out.println("---------------------------------------------------------------------------");
+            System.out.println("The number of games played is " + numGames);
+            System.out.println("Average turns per game = " + totalTurns/numGames);
+            System.out.println(color.getStratName() + "'s percentage of wins = " + (double)players[1]/(double)numGames + " with an average of " + p1TotalDraws/numGames + " number of draws per game");
+            System.out.println(color1.getStratName() +"'s percentage of wins = " + (double)players[2]/(double)numGames + " with an average of " + p2TotalDraws/numGames + " number of draws per game");
+    }
+    
+    public void PassiveVsRandom(int numGames)
+    {
+        int winner;
+        int[] players = new int[3];
+        int totalTurns = 0;
+        int p1TotalDraws = 0;
+        int p2TotalDraws = 0;
+        
+
+        Strategy color = new PassiveStrategy();
+        Strategy color1 = new RandomStrategy();
+       
+        Game game = new Game();
+        for(int i = 0; i < numGames; i++)
+        {
+            game = new Game();
+            winner = game.playGame(color, color1);
+            players[winner] += 1; // add to histogram
+            totalTurns += game.getNumTurns();
+            p1TotalDraws += game.getP1Draws();
+            p2TotalDraws += game.getP2Draws();
+            
+            //System.out.println("Strategy " + winner + " wins!");
+        }
+            System.out.println("\n");
+            System.out.println("This was a " + color.getStratName() + " vs " + color1.getStratName() + " game, where " + color.getStratName() + " goes first.");
+            System.out.println("Here's some Statistics:");
+            System.out.println("---------------------------------------------------------------------------");
+            System.out.println("The number of games played is " + numGames);
+            System.out.println("Average turns per game = " + totalTurns/numGames);
+            System.out.println(color.getStratName() + "'s percentage of wins = " + (double)players[1]/(double)numGames + " with an average of " + p1TotalDraws/numGames + " number of draws per game");
+            System.out.println(color1.getStratName() +"'s percentage of wins = " + (double)players[2]/(double)numGames + " with an average of " + p2TotalDraws/numGames + " number of draws per game");
+    }
+}
